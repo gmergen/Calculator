fa73974e1f4a4f527bab70b6ad14c1a997cfa465
Griffin <gmergen1996@gmail.com>
2019-02-06 13:11:11 -0600

    Updated Addtest

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTest
+    {
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(-2, -4, -6)]
+        public void AddTest(int a, int b, int expected)
+        {
+            //Arrange 
+            Calculator calculator = new Calculator();
+
+            //Act
+            int actual = calculator.Add(a, b);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
