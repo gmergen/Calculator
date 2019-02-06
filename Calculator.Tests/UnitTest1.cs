67b67e3e2b68a9fc5b9cbcc87e65361528764422
Griffin <gmergen1996@gmail.com>
2019-02-06 13:58:09 -0600

    updated tests

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -7,8 +7,6 @@ namespace Calculator.Tests
     {
         [Theory]
         [InlineData(1, 1, 2)]
-        [InlineData(2, 2, 4)]
-        [InlineData(-2, -4, -6)]
         public void AddTest(int a, int b, int expected)
         {
             //Arrange 
@@ -23,8 +21,6 @@ namespace Calculator.Tests
 
         [Theory]
         [InlineData(2, 1, 1)]
-        [InlineData(6, 3, 3)]
-        [InlineData(10, 5, 5)]
         public void SubtractTest(int a, int b, int expected)
         {
             Calculator calculator = new Calculator();
